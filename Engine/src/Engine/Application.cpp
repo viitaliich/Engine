cc22eab4f2f4e2cc10f02cb08b21b1f63e3b5b8c
Vitalii Klimov <vitklim11@gmail.com>
2022-05-30 12:46:21 +0300

    Event system implementation (glfw callbacks)

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -2,14 +2,16 @@
 
 #include "Application.h"
 
-#include "Engine/Events/ApplicationEvent.h"
-
 #include <GLFW/glfw3.h>
+
+// this ???
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1) 
 
 namespace Engine {
 
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
@@ -25,6 +27,21 @@ namespace Engine {
 			m_Window->OnUpdate();
 		}
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		//???
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		EG_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Engine/src/Engine/Application.h
+++ Engine/src/Engine/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Engine/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Engine {
@@ -14,7 +15,11 @@ namespace Engine {
 									// when this destructor is called, Sandbox destructor must be called too	???
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
